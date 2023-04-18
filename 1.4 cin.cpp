6e0d5dc9e67d977912cc7e509274dda49868492c
lionelchoonsik <128296431+lionelchoonsik@users.noreply.github.com>
2023-04-19 01:20:01 +0900

    1.4 cin

---
+++ 1.4 cin.cpp
@@ -0,0 +1,18 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x=1;
+    cout << "before your input is " << x << " 숫자를 입력하라 : "<<endl;
+    cin >> x; //cin은 input 과 같은 역할
+    cout << "after your input is "<<x << endl;
+    return 0;
+}
+/*
+before your input is 1 숫자를 입력하라 :
+6
+after your input is 6
+*/
+
