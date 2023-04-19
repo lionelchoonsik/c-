a4cde6be34d33693a45bc0c32102e8243b8bc4c2
lionelchoonsik <128296431+lionelchoonsik@users.noreply.github.com>
2023-04-19 23:31:03 +0900

    Add files via upload

---
+++ 1.6키워드와 식별자이름짓기.cpp
@@ -0,0 +1,16 @@
+﻿// 1.6키워드와 식별자이름짓기.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+
+void main() //void는 return 값이 없을때 씀
+{
+
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
